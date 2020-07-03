10dfce4551803d527f8ab1dc149155413cea8c74
rianjanuarian <55471319+rianjanuarian@users.noreply.github.com>
2020-07-03 10:11:02 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 	
-	$server		= "3306"; // sesuaikan alamat server anda
-	$user		= "kelompok1e"; // sesuaikan user web server anda
-	$password	= "kelompok1e"; // sesuaikan password web server anda
-	$database	= "u1011496_pelita"; // sesuaikan database web server anda
+	$server		= "localhost"; // sesuaikan alamat server anda
+	$user		= "root"; // sesuaikan user web server anda
+	$password	= ""; // sesuaikan password web server anda
+	$database	= "pelitanew"; // sesuaikan database web server anda
 	
 	$connect = mysql_connect($server, $user, $password) or die ("Tidak bisa terhubung!");
 	mysql_select_db($database) or die ("Database belum siap!");
